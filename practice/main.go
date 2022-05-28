b814db059810ca7fc18787c6577b07a292d9c130
Jay Barbhaiya <jaybarb1990@gmail.com>
2022-05-28 15:11:19 +0200

    pointer basics

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,16 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 33
+	var p = &anInt
+	fmt.Println("Memory address - pointer p:", p)
+	fmt.Println("Value - pointer p:", *p)
+
+	aFloat := 33.33
+	pointer1 := &aFloat
+	fmt.Println("aFloat", *pointer1)
+
+	*pointer1 = *pointer1 / 11
+	fmt.Println("aFloat pointer", *pointer1)
+	fmt.Println("aFloat value changed via changing pointer1", aFloat)
 }
